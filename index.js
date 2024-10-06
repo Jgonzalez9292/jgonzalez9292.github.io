a2fdefa08755221e33317d798f118738f259e054
Jgonzalez9292 <132224709+Jgonzalez9292@users.noreply.github.com>
2024-10-05 21:45:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,24 +28,3 @@ else{
     timeOfDay = 'evening'
 }
 document.getElementById("timeOfDay").textContent = timeOfDay;
-
-
-//checks if form is completed
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contactForm');
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-
-        if (name === '' || email === '' || message === '') {
-            alert('Please fill in all fields');
-        } else {
-            alert('Form submitted successfully!');
-            form.reset();
-        }
-    });
-});
