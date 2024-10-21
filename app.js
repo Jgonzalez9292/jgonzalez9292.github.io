7c7283d6b025d794585ba8cfb3b46ce12fbb33f5
Jgonzalez9292 <132224709+Jgonzalez9292@users.noreply.github.com>
2024-10-20 21:36:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ app.post('/', (req, res) => {
     });
 });
 
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
